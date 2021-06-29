305488f33ed1c1ae7a6774d333cf9a6007a8a1db
Grégory <contact@gregory-gerard.dev>
2021-06-29 20:12:09 +0200

    Ide helper

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -21,16 +21,23 @@ namespace App\Models{
  * @property string $code
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property int $size
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Transfer onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer query()
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereCode($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereContent($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereOriginalName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereSize($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Transfer whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Transfer withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Transfer withoutTrashed()
  */
 	class Transfer extends \Eloquent {}
 }
